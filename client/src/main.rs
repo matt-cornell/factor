d43707982c2fb48276fc50ac842fa9eaa7411195
Matthew Cornell <mcornell400708@gmail.com>
2024-12-30 00:06:56 -0500

    Fit game builds to the parent canvas for web builds
    Signed-off-by: Matthew Cornell <mcornell400708@gmail.com>

--- client/src/main.rs
+++ client/src/main.rs
@@ -3,7 +3,14 @@ use factor_client::{default_style, ClientPlugin};
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                fit_canvas_to_parent: true,
+                title: "Factor- Client Only".into(),
+                ..default()
+            }),
+            ..default()
+        }))
         .add_plugins(ClientPlugin {
             can_use_singleplayer: false,
             egui_style: default_style(),
--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,14 @@ use factor_server::ServerPlugin;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                fit_canvas_to_parent: true,
+                title: "Factor".into(),
+                ..default()
+            }),
+            ..default()
+        }))
         .add_plugins(ServerPlugin::default())
         .add_plugins(ClientPlugin {
             can_use_singleplayer: true,
